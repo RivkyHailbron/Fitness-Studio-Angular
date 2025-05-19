ebdacab31874c6ac43297d9900379811f2e38ac8
RivkyHailbron <rivky2712@gmail.com>
2025-05-19 18:55:08 +0300

    gym - teacher finish

--- src/app/lesson-details/lesson-details.component.ts
+++ src/app/lesson-details/lesson-details.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
-import { Lesson } from '../gymteacher/gymteacher.component';
 import { NgIf } from '@angular/common';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
